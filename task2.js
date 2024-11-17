e3670a706268e7d289bfa924ef4ef163eae0e84d
mark <markqc@users.noreply.github.com>
2024-11-17 11:23:42 +0800

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,4 +1,4 @@
-// Employees failing in aggregate
+// Find employees who failed in aggregate (term1 + term2 + term3) 
 db.employees.find({
   $where: "this.results.reduce((sum, term) => sum + term.score, 0) < 111"
 });
